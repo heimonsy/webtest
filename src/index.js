ac746bd0d967509fab55055f34517cdb537f9839
heimonsy <heimonsy@gmail.com>
2023-01-24 11:00:30 -0700

    asdf

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Invalid>
+      <Other></Other>
+    </Invalid>
   </React.StrictMode>
 );
 
